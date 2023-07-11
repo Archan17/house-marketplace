168e46b3e2828bb33d646a6ad81af000d9034d49
Archan Pandya <pandyaarchan17504@gmail.com>
2023-07-11 22:31:04 +0530

    Completed CreateListings

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
+import Createlistings from "./pages/Createlistings";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<Createlistings />} />
         </Routes>
         <Navbar />
       </BrowserRouter>
